aa5cd8d0a03a591d8b24b188d85a09dd9e5daa7c
Jesus <lucas.r2006@aluno.ifsc.edu.br>
2024-11-29 09:43:51 -0300

    salvando

---
+++ Corrida/Assets/Fontes/Fontes.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fontes : MonoBehaviour
+{
+    /*
+    https://assetstore.unity.com/packages/2d/environments/free-2d-cartoon-parallax-background-205812
+    https://craftpix.net/freebies/city-man-pixel-art-character-sprite-sheets/
+    https://pzuh.itch.io/free-desert-platformer-tileset
+
+
+    */
+}
