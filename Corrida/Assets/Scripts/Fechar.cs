a1a0f35e703b547cdeedab1bda356ee37fedad4f
Jesus <lucas.r2006@aluno.ifsc.edu.br>
2024-12-06 10:21:28 -0300

    salvar

---
+++ Corrida/Assets/Scripts/Fechar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fechar : MonoBehaviour
+{
+    public void fechar()
+    {
+        Application.Quit();
+    }
+}
